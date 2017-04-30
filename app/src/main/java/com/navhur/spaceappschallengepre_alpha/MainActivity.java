ca45724387301f35d48d5b7bb03a57ca6201d80c
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2017-04-30 11:18:20 -0600

    Retouches

--- app/src/main/java/com/navhur/spaceappschallengepre_alpha/MainActivity.java
+++ app/src/main/java/com/navhur/spaceappschallengepre_alpha/MainActivity.java
@@ -1,12 +1,19 @@
 package com.navhur.spaceappschallengepre_alpha;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.app.Fragment;
+import android.support.v4.app.NotificationBuilderWithBuilderAccessor;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,9 +23,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +52,14 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+            }
+        }, 100);
     }
 
     @Override
@@ -72,6 +91,24 @@ public class MainActivity extends AppCompatActivity
             return true;
         }
 
+        if (id == R.id.call_an_ambulance) {
+            phoneIntent.setData(Uri.parse("tel:*102"));
+            startActivity(phoneIntent);
+            return true;
+        }
+        if (id == R.id.call_the_firefighters) {
+            phoneIntent.setData(Uri.parse("tel:*911"));
+            startActivity(phoneIntent);
+            return true;
+        }
+        if (id == R.id.call_the_police) {
+            phoneIntent.setData(Uri.parse("tel:*118"));
+            startActivity(phoneIntent);
+            return true;
+        }
+
+
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -85,7 +122,7 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_home) {
             fragmentManager.beginTransaction()
                     .replace(R.id.content_main
-                    , new HomeFragment())
+                            , new HomeFragment())
                     .commit();
 
         } else if (id == R.id.nav_beaches) {
@@ -116,4 +153,53 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void sendNotification(View view) {
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this);
+
+//Create the intent that’ll fire when the user taps the notification//
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.ineter.gob.ni/articulos/areas-tecnicas/geofisica/monitoreo-de-sismos-en-tiempo-real.html"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        mBuilder.setContentIntent(pendingIntent);
+
+        mBuilder.setSmallIcon(R.drawable.ic_palm_tree);
+        mBuilder.setContentTitle("Alerta de Sismo");
+        mBuilder.setContentText("Sismo detectado tome precaucion en la playa");
+        mBuilder.setColor(getResources().getColor(R.color.colorPrimaryOrange));
+
+        NotificationManager mNotificationManager =
+
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(001, mBuilder.build());
+    }
+
+    public void sendNotification2(View view) {
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this);
+
+//Create the intent that’ll fire when the user taps the notification//
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.ineter.gob.ni/articulos/areas-tecnicas/geofisica/monitoreo-de-sismos-en-tiempo-real.html"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        mBuilder.setContentIntent(pendingIntent);
+
+        mBuilder.setSmallIcon(R.drawable.ic_waves);
+        mBuilder.setContentTitle("Alerta de Tsunami");
+        mBuilder.setContentText("Tsunami detectado alejese de la playa");
+        mBuilder.setColor(getResources().getColor(R.color.colorPrimary));
+
+        NotificationManager mNotificationManager =
+
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(001, mBuilder.build());
+    }
+
 }
